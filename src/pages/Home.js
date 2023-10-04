fc4901ce15d7dda1cb76e7f0b4e5f263065ee4e3
abhishek781 <20abhishek16@gmail.com>
2023-10-04 11:42:20 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import SearchForm from '../components/SearchForm';
+import WordDetails from '../components/WordDetails';
+
+function Home() {
+  return (
+    <div>
+      <h1>Dictionary App</h1>
+      <SearchForm />
+      <WordDetails />
+    </div>
+  );
+}
+
+export default Home;
+
+
+
+
